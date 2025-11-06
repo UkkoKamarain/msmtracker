da016c4e4cba4860f1bf3411f4a0bac2a2a57f0f
ukko.kamarainen@myy.haaga-helia.fi <ukko.kamarainen@myy.haaga-helia.fi>
2025-11-06 17:55:27 +0200

    Removing gson dependencies to switch to jackson, w.i.p. (not reading elements and islands into monsters)

--- src/main/java/tools/msm/b_tracking_list/BTrackingListApplication.java
+++ src/main/java/tools/msm/b_tracking_list/BTrackingListApplication.java
@@ -1,15 +1,10 @@
 package tools.msm.b_tracking_list;
 
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import org.slf4j.Logger;
-
-import javax.swing.text.html.parser.Element;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -17,9 +12,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.stream.JsonReader;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import tools.msm.b_tracking_list.domain.MsmElement;
 import tools.msm.b_tracking_list.domain.MsmElementRepository;
@@ -46,63 +40,36 @@ public class BTrackingListApplication {
 
 	@Bean
 	public CommandLineRunner msmTrackerRunner(
-		MsmMonsterRepository mR,
-		MsmIslandRepository iR,
-		MsmElementRepository eR
-	) {
-		
+			MsmMonsterRepository mR,
+			MsmIslandRepository iR,
+			MsmElementRepository eR) {
+
 		return (args) -> {
-		
-			// // CREATING SAMPLE ELEMENTS (placeholder)
-			// MsmElement stone = new MsmElement("Stone");
-			// MsmElement water = new MsmElement("Water");
-			// MsmElement cold = new MsmElement("Cold");
-			// MsmElement plant = new MsmElement("Plant");
-			// MsmElement air = new MsmElement("Air");
-
-			// // Islands
-			// MsmIsland plantIsland = new MsmIsland("Plant", Arrays.asList(stone,water,cold,plant));
-			// MsmIsland coldIsland = new MsmIsland("Cold", Arrays.asList(air,water,cold,plant));
-
-			// // Monsters
-			// MsmMonster noggin = new MsmMonster("Noggin",
-			// "Noggin + Quad",
-			// Arrays.asList(plantIsland),
-			// Arrays.asList(stone),
-			// Arrays.asList(Duration.ofSeconds(5),Duration.ofSeconds(3),Duration.ofSeconds(4),Duration.ofSeconds(3)));
-			// MsmMonster mammott = new MsmMonster("Mammott",
-			// "Mammott + Quad",
-			// Arrays.asList(plantIsland,coldIsland),
-			// Arrays.asList(cold),
-			// Arrays.asList(Duration.parse("PT2M"),Duration.parse("PT1M30S"),Duration.parse("PT1M48S"),Duration.parse("PT1M18S")));
 
 			// Logins
 			log.info("Creating users.");
 			MsmUser user1 = new MsmUser("user", "ea/u5KKXV6EVyZKETumONM7p5hqQvRqcfNNj/Z5513OS2E.ZCoC", "USER");
-			MsmUser user2 = new MsmUser("admin", "$2a$12$N3BS0FKBUnzdg80TcsXKr.3k3AQlx3zwcmNA8T7z7d/gILLQF8KTi","ADMIN");
+			MsmUser user2 = new MsmUser("admin", "$2a$12$N3BS0FKBUnzdg80TcsXKr.3k3AQlx3zwcmNA8T7z7d/gILLQF8KTi",
+					"ADMIN");
 
 			log.info("Inserting users.");
 			uR.save(user1);
-			uR.save(user2);	
+			uR.save(user2);
 			log.info("Users inserted.");
 
-			log.info("Creating Gsons.");
-			Gson iGson = new GsonBuilder()
-			.registerTypeAdapter(MsmIsland.class,new MsmIslandDeserializer(eR))
-			.create();
-			Gson mGson = new GsonBuilder()
-			.registerTypeAdapter(MsmIsland.class,new MsmMonsterDeserializer(eR, iR))
-			.create();
-			Gson gson = new Gson();
-			log.info("Gsons created.");
+			ObjectMapper mapper = new ObjectMapper();
+			mapper.findAndRegisterModules();			
+
+			File elementFile = new File("src/main/resources/elements.json");
+			File islandFile = new File("src/main/resources/islands.json");
+			File monsterFile = new File("src/main/resources/monsters.json");
 
 			log.info("Trying to parse files:");
 			try {
-				log.info("Reading elements.");
-				JsonReader eReader = new JsonReader(new FileReader("b_tracking_list/src/main/resources/elements.json"));
+				// log.info("Reading elements.");
 				log.info("Parsing elements.");
-				List<MsmElement> eList = gson.fromJson(eReader, MsmElement[].class);
-				eReader.close();
+				List<MsmElement> eList = mapper.readValue(elementFile, new TypeReference<List<MsmElement>>() {
+				});
 				log.info("Elements parsed.");
 
 				log.info("Inserting elements.");
@@ -111,11 +78,12 @@ public class BTrackingListApplication {
 				}
 				log.info("Elements inserted.");
 
-				log.info("Reading islands.");
-				JsonReader iReader = new JsonReader(new FileReader("b_tracking-list/src/main/resources/islands.json"));
+				// log.info("Reading islands.");
+				// JsonReader iReader = new JsonReader(new FileReader(islandFile));
 				log.info("Parsing islands.");
-				List<MsmIsland> iList = iGson.fromJson(iReader, MsmIsland[].class);
-				iReader.close();
+				List<MsmIsland> iList = mapper.readValue(islandFile, new TypeReference<List<MsmIsland>>() {
+				});
+				// iReader.close();
 				log.info("Islands parsed.");
 
 				log.info("Inserting islands.");
@@ -123,12 +91,12 @@ public class BTrackingListApplication {
 					iR.save(iList.get(i));
 				}
 				log.info("Islands Inserted.");
-				
-				log.info("Reading monsters.");
-				JsonReader mReader = new JsonReader(new FileReader("b_tracking_list/src/main/resources/static/monsters.json"));
+
+				// log.info("Reading monsters.");
+				// JsonReader mReader = new JsonReader(new FileReader(monsterFile));
 				log.info("Parsing monsters.");
-				List<MsmMonster> mList = mGson.fromJson(mReader, MsmMonster[].class);
-				mReader.close();
+				List<MsmMonster> mList = mapper.readValue(monsterFile, new TypeReference<List<MsmMonster>>(){});
+				// mReader.close();
 				log.info("Monsters parsed.");
 
 				log.info("Inserting monsters.");
@@ -145,7 +113,7 @@ public class BTrackingListApplication {
 			} catch (IOException e) {
 				System.out.println(e);
 				log.info("Error: IO.");
-			} 
+			}
 
 			log.info("Log end.");
 		};
